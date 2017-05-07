b6974303140f93aa4f11f68a355fb10f1f807328
iceben <iceben@sch.bme.hu>
2017-05-07 13:44:42 +0200

    Tests for embeddedtestkitdriver

--- neo4j-embedded-testkit-driver/src/test/java/neo4j/driver/testkit/EmbeddedTestkitDriverTest.java
+++ neo4j-embedded-testkit-driver/src/test/java/neo4j/driver/testkit/EmbeddedTestkitDriverTest.java
@@ -1,11 +1,17 @@
 package neo4j.driver.testkit;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
 import org.junit.Test;
+import org.neo4j.driver.v1.AccessMode;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
 import org.neo4j.driver.v1.Transaction;
+import org.neo4j.graphdb.GraphDatabaseService;
 
 import neo4j.driver.util.PrettyPrinter;
 
@@ -58,4 +64,27 @@ public class EmbeddedTestkitDriverTest {
 			}
 		}
 	}
+	
+	@Test
+	public void test0() { //EmbeddedTestkitDriver tests
+		GraphDatabaseService gds = null;
+		File f = null;
+		Driver drivertest = new EmbeddedTestkitDriver(f); //create with a file
+		GraphDatabaseService returned=((EmbeddedTestkitDriver) drivertest).getUnderlyingDatabaseService();
+		assertTrue(returned!=null); //we created with a null file, but we successfully got a gds
+		try (Session session1 = drivertest.session("Bookmark")) {
+		} catch(UnsupportedOperationException e){} //catch one exception
+		AccessMode am = null;
+		try (Session session2 = drivertest.session(am,"Bookmark2")) {
+		} catch(UnsupportedOperationException e){} //catch another exception
+		
+		try (Driver driver = new EmbeddedTestkitDriver(gds)) { //create with a gds
+			if(!driver.isEncrypted())
+			try (Session session = driver.session()) { //create a session, other functions already tested
+			}
+		}
+	}
+	
+	
+	
 }
